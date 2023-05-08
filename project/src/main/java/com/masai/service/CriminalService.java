4d7a7b4995a48df1cc0f524f9424eeea8d71d40b
Rude00 <subiradhikari181@gmail.com>
2023-05-08 20:52:26 +0530

    day04

---
+++ project/src/main/java/com/masai/service/CriminalService.java
@@ -0,0 +1,16 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.Entity.Criminal;
+import com.masai.Exception.AlreadyExistException;
+import com.masai.Exception.NoRecordFoundException;
+import com.masai.Exception.SomethingWentWrongFetchingException;
+import com.masai.Exception.SomethingWrongAddingException;
+
+public interface CriminalService {
+	void addCriminal(Criminal criminal)throws SomethingWrongAddingException,AlreadyExistException;
+	List<Criminal> getCriminalList()throws SomethingWentWrongFetchingException, NoRecordFoundException;
+	void updateCriminal(Criminal criminal)throws SomethingWentWrongFetchingException, NoRecordFoundException;
+	void deleteCriminal(int id)throws SomethingWentWrongFetchingException, NoRecordFoundException;
+}
